4c5f10635d3375e4812af8a996e9d88b145f39e6
ruchira403 <it22278630@my.sliit.lk>
2025-04-25 20:20:25 +0530

    modify the userservice file by adding findall ,findall by id

--- psn-backend/src/main/java/com/linhtch90/psnbackend/service/UserService.java
+++ psn-backend/src/main/java/com/linhtch90/psnbackend/service/UserService.java
@@ -22,3 +22,34 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptEncoder;
+
+    public ResponseObjectService findAll() {
+        ResponseObjectService responseObj = new ResponseObjectService();
+        responseObj.setPayload(userRepo.findAll());
+        responseObj.setStatus("success");
+        responseObj.setMessage("success");
+        return responseObj;
+    }
+
+    public ResponseObjectService findById(String id) {
+        ResponseObjectService responseObj = new ResponseObjectService();
+        Optional<UserEntity> optUser = userRepo.findById(id);
+        if (optUser.isEmpty()) {
+            responseObj.setStatus("fail");
+            responseObj.setMessage("user id: " + id + " not existed");
+            responseObj.setPayload(null);
+            return responseObj;
+        } else {
+            responseObj.setPayload(optUser.get());
+            responseObj.setStatus("success");
+            responseObj.setMessage("success");
+            return responseObj;
+        }
+    }
