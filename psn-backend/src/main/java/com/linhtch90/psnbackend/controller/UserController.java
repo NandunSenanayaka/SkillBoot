62b914b5146227b635e67ba11e0ee6ced71b1594
ruchira403 <it22278630@my.sliit.lk>
2025-04-25 20:09:02 +0530

    add the postmapping for the usercontroller file

--- psn-backend/src/main/java/com/linhtch90/psnbackend/controller/UserController.java
+++ psn-backend/src/main/java/com/linhtch90/psnbackend/controller/UserController.java
@@ -39,3 +39,54 @@ public class UserController {
 
     @Autowired
     private UserRepository userRepo;
+
+    @PostMapping("/users")
+    public ResponseEntity<ResponseObjectService> findAllUsers() {
+        return new ResponseEntity<ResponseObjectService>(userService.findAll(), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/profile")
+    public ResponseEntity<ResponseObjectService> findById(@RequestBody IdObjectEntity inputId) {
+        return new ResponseEntity<ResponseObjectService>(userService.findById(inputId.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/follow")
+    public ResponseEntity<ResponseObjectService> followUser(@RequestBody DoubleIdObjectEntity doubleId) {
+        return new ResponseEntity<ResponseObjectService>(userService.followUser(doubleId), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/unfollow")
+    public ResponseEntity<ResponseObjectService> unfollowUser(@RequestBody DoubleIdObjectEntity doubleId) {
+        return new ResponseEntity<ResponseObjectService>(userService.unfollowUser(doubleId), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/getfollowing")
+    public ResponseEntity<ResponseObjectService> findFollowing(@RequestBody IdObjectEntity inputId) {
+        return new ResponseEntity<ResponseObjectService>(userService.findFollowing(inputId.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/getfollower")
+    public ResponseEntity<ResponseObjectService> findFollower(@RequestBody IdObjectEntity inputId) {
+        return new ResponseEntity<ResponseObjectService>(userService.findFollower(inputId.getId()), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/save")
+    public ResponseEntity<ResponseObjectService> saveUser(@RequestBody UserEntity inputUser) {
+        return new ResponseEntity<ResponseObjectService>(userService.saveUser(inputUser), HttpStatus.OK);
+    }
+
+    @PostMapping("/users/signin")
+    public ResponseEntity<ResponseObjectService> userSignIn(@RequestBody UserSignInEntity inputUser) {
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(inputUser.getEmail(), inputUser.getPassword()));
+            String token = jwtUtil.generateToken(inputUser.getEmail());
+            
+            Optional<UserEntity> optUser = userRepo.findByEmail(inputUser.getEmail());
+            UserEntity user = optUser.get();
+            user.setPassword("");
+            return new ResponseEntity<ResponseObjectService>(new ResponseObjectService("success", "authenticated", new AuthorizedEntity(user, token)), HttpStatus.OK);
+        } catch (Exception ex) {
+            return new ResponseEntity<ResponseObjectService>(new ResponseObjectService("fail", "unauthenticated", null), HttpStatus.OK);
+        }
+    }
+
